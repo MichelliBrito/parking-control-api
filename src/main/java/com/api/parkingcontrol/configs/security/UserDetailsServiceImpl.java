6052654aaed6f229ef9c67e468a1163cef21257e
michellibrito <michellidibrito@gmail.com>
2022-07-12 11:04:26 -0300

    remove imports

--- src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
+++ src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
@@ -2,7 +2,6 @@ package com.api.parkingcontrol.configs.security;
 
 import com.api.parkingcontrol.models.UserModel;
 import com.api.parkingcontrol.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
